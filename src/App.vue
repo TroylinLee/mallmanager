67136ddfa101123b852aa4fb45cad4b709c58f5f
TroylinLee <361719496@qq.com>
2020-12-12 01:10:13 +0800

    新建登录分支新建登录组件配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="id">
-    
+    <router-view></router-view>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,14 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
+const Login = () => import('@/components/login/Login')
+
 const routes = [
-  
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  }
 ]
 
 const router = new VueRouter({
